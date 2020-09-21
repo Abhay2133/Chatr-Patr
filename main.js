da6764f612db25688f9b3fcaaadbc076b2763e74
Abhay2133 <abhaybishthestudent@gmail.com>
2020-09-22 04:20:27 +0530

    ...

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+//console.log('Hello World!');
+const se = (key, val) => {
+        switch (key) {
+            
+ case 'i': return document.getElementById(val);
+    break;
+ case 'c': return document.getElementsByClassName(val);
+    break;
+ case 't': return document.getElementsByTagName(val);
+    break;
+    
+        }
+        
+        return 0;
+    }
+    
+window.onload = function (){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
